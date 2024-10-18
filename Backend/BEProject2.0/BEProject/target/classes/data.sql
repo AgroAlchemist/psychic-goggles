87515536f01761303f5e18b3d6b7f31679a86edb
SahilBheke <SahilBheke03@gmail.com>
2024-10-18 14:31:26 +0530

    H2 Database Working

--- Backend/BEProject2.0/BEProject/target/classes/data.sql
+++ Backend/BEProject2.0/BEProject/target/classes/data.sql
@@ -1,14 +1,10 @@
 insert into products(price,stock_Quantity,id, description, name)
-values(55.2, 2, 10001, 'For Harvesting and Cultivating Crops', 'Tractor');
+values(55.2, 2, 1, 'For Harvesting and Cultivating Crops', 'Tractor');
 
 insert into products(price,stock_Quantity,id, description, name)
-values(10, 10, 10002, 'Reaping, Reaping and Reaping', 'Combines and Forage Harvesters');
+values(10, 10, 2, 'Reaping, Reaping and Reaping', 'Combines and Forage Harvesters');
 
 insert into products(price,stock_Quantity,id, description, name)
-values(100, 4, 10003, 'they spread fertilizer across a field. While there are fertilizer spreaders that can be run separately', 'Fertilizer Spreaders');
+values(100, 4, 3, 'they spread fertilizer across a field. While there are fertilizer spreaders that can be run separately', 'Fertilizer Spreaders');
 
--- insert into products(price,stock_Quantity,id, description, name)
--- values(55.2, 2, 10001, 'Harvesting', 'Tractor');
 
--- insert into products(price,stock_Quantity,id, description, name)
--- values(55.2, 2, 10001, 'Harvesting', 'Tractor');
