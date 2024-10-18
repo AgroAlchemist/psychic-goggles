51c488698f65958a69458be881a234b758d29736
SahilBheke <SahilBheke03@gmail.com>
2024-10-18 05:40:33 +0530

    Data SQL updated with seed data

--- Backend/BEProject2.0/BEProject/target/classes/data.sql
+++ Backend/BEProject2.0/BEProject/target/classes/data.sql
@@ -1,11 +1,11 @@
 insert into products(price,stock_Quantity,id, description, name)
-values(55.2, 2, 10001, 'For Harvesting and Cultivating Crops', 'Tractor');
+values(55.2, 2, 10001, 'Good Qulaity Seeds', 'Papaya');
 
 insert into products(price,stock_Quantity,id, description, name)
-values(10, 10, 10002, 'Reaping, Reaping and Reaping', 'Combines and Forage Harvesters');
+values(10, 10, 10002, 'Usually grown in north', 'Apple');
 
 insert into products(price,stock_Quantity,id, description, name)
-values(100, 4, 10003, 'they spread fertilizer across a field. While there are fertilizer spreaders that can be run separately', 'Fertilizer Spreaders');
+values(100, 4, 10003, 'Majory grown in South India', 'Banana');
 
 -- insert into products(price,stock_Quantity,id, description, name)
 -- values(55.2, 2, 10001, 'Harvesting', 'Tractor');
